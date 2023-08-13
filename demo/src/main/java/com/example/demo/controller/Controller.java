9260f27236133edf4355f8112b74499831ff30be
omega1u123 <gvardian2810@gmail.com>
2023-08-13 17:19:07 +0300

    fix some errors

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.Service.TaskService;
-import com.example.demo.Service.UserService;
 import com.example.demo.store.entity.TaskEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- demo/src/main/java/com/example/demo/store/entity/UserEntity.java
+++ demo/src/main/java/com/example/demo/store/entity/UserEntity.java
@@ -2,7 +2,6 @@ package com.example.demo.store.entity;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
